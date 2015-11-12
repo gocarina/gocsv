fa9b20c6c788706dfd89c413b76775f31fdc34b0
Herbert Chang <herbert@clypd.com>
2015-11-12 11:50:23 -0500

    MAIN-11864: more robust tests

--- decode_test.go
+++ decode_test.go
@@ -25,4 +25,17 @@ func Test_maybeMissingStructFields(t *testing.T) {
 	if err := maybeMissingStructFields(structTags, goodHeaders); err != nil {
 		t.Fatal(err)
 	}
+
+	// extra headers, but all structtags match; expect no error
+	moarHeaders := append(goodHeaders, "qux", "quux", "corge", "grault")
+	if err := maybeMissingStructFields(structTags, moarHeaders); err != nil {
+		t.Fatal(err)
+	}
+
+	// not all structTags match, but there's plenty o' headers; expect
+	// error
+	mismatchedHeaders := []string{"foo", "qux", "quux", "corgi"}
+	if err := maybeMissingStructFields(structTags, mismatchedHeaders); err == nil {
+		t.Fatal("expected an error, but no error found")
+	}
 }
