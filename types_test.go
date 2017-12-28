23cb729ff127e2b6f3ba7c6e1b0e393cc2a68f32
Andrei Mackenzie <andrei@clypd.com>
2017-12-28 16:10:31 -0500

    Apply the same pattern to improve 'marshall'
    Before:
    goos: darwin
    goarch: amd64
    pkg: github.com/clyphub/gocsv
    Benchmark_marshall_TypeMarshaller-8     10000000               230 ns/op
    Benchmark_marshall_TextMarshaller-8      5000000               377 ns/op
    Benchmark_marshall_Stringer-8            5000000               272 ns/op
    PASS
    ok      github.com/clyphub/gocsv        6.471s
    
    After:
    goos: darwin
    goarch: amd64
    pkg: github.com/clyphub/gocsv
    Benchmark_marshall_TypeMarshaller-8     10000000               162 ns/op
    Benchmark_marshall_TextMarshaller-8     10000000               208 ns/op
    Benchmark_marshall_Stringer-8           10000000               176 ns/op
    PASS
    ok      github.com/clyphub/gocsv        6.061s

--- types_test.go
+++ types_test.go
@@ -14,6 +14,10 @@ func (s *sampleTypeUnmarshaller) UnmarshalCSV(val string) error {
 	return nil
 }
 
+func (s sampleTypeUnmarshaller) MarshalCSV() (string, error) {
+	return s.val, nil
+}
+
 type sampleTextUnmarshaller struct {
 	val []byte
 }
@@ -21,6 +25,16 @@ type sampleTextUnmarshaller struct {
 func (s *sampleTextUnmarshaller) UnmarshalText(text []byte) error {
 	s.val = text
 	return nil
+}
+
+func (s sampleTextUnmarshaller) MarshalText() ([]byte, error) {
+	return s.val, nil
+}
+
+type sampleStringer string
+
+func (s sampleStringer) String() string {
+	return string(s)
 }
 
 func Benchmark_unmarshall_TypeUnmarshaller(b *testing.B) {
@@ -39,6 +53,39 @@ func Benchmark_unmarshall_TextUnmarshaller(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		if err := unmarshall(val, "foo"); err != nil {
 			b.Fatalf("unmarshall error: %s", err.Error())
+		}
+	}
+}
+
+func Benchmark_marshall_TypeMarshaller(b *testing.B) {
+	sample := sampleTypeUnmarshaller{"foo"}
+	val := reflect.ValueOf(&sample)
+	for n := 0; n < b.N; n++ {
+		_, err := marshall(val)
+		if err != nil {
+			b.Fatalf("marshall error: %s", err.Error())
+		}
+	}
+}
+
+func Benchmark_marshall_TextMarshaller(b *testing.B) {
+	sample := sampleTextUnmarshaller{[]byte("foo")}
+	val := reflect.ValueOf(&sample)
+	for n := 0; n < b.N; n++ {
+		_, err := marshall(val)
+		if err != nil {
+			b.Fatalf("marshall error: %s", err.Error())
+		}
+	}
+}
+
+func Benchmark_marshall_Stringer(b *testing.B) {
+	sample := sampleStringer("foo")
+	val := reflect.ValueOf(&sample)
+	for n := 0; n < b.N; n++ {
+		_, err := marshall(val)
+		if err != nil {
+			b.Fatalf("marshall error: %s", err.Error())
 		}
 	}
 }
